f6c83cd9cc1474e3d85fc2fdb583ab7375d830e0
Mololuwa Segilola <72552301+mololuwa10@users.noreply.github.com>
2025-01-20 18:01:48 +0000

    "implemented a modal feature for add habit"

--- app/AddHabit/page.tsx
+++ app/AddHabit/page.tsx
@@ -1,5 +1,5 @@
+"use client";
 import { Button } from "@/components/ui/button";
-import React from "react";
 
 export default function AddHabit() {
 	return (
@@ -14,61 +14,11 @@ export default function AddHabit() {
 					</label>
 					<input
 						type="text"
-						id="firstname"
+						id="habitname"
 						className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
 					/>
 				</div>
-				<div className="mb-4">
-					<label
-						htmlFor="lastname"
-						className="block text-gray-700 text-sm font-bold mb-2"
-					>
-						Last Name
-					</label>
-					<input
-						type="text"
-						id="lastname"
-						className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-					/>
-				</div>
-				<div className="mb-4">
-					<label
-						htmlFor="email"
-						className="block text-gray-700 text-sm font-bold mb-2"
-					>
-						Email Address
-					</label>
-					<input
-						type="email"
-						id="email"
-						className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-					/>
-				</div>
-				<div className="mb-4">
-					<label
-						htmlFor="phone"
-						className="block text-gray-700 text-sm font-bold mb-2"
-					>
-						Phone Number
-					</label>
-					<input
-						type="tel"
-						id="phone"
-						className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-					/>
-				</div>
-				<div className="mb-4">
-					<label
-						htmlFor="address"
-						className="block text-gray-700 text-sm font-bold mb-2"
-					>
-						Address
-					</label>
-					<textarea
-						id="address"
-						className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-					/>
-				</div>
+
 				<Button className="mt-4 text-white px-6 py-2 rounded hover:bg-gray-950 hover:scale-110 animation ease-in duration-200">
 					Save
 				</Button>
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,17 +1,54 @@
-import { Plus } from "lucide-react";
-import Link from "next/link";
-import React from "react";
+"use client";
+import AddHabit from "@/app/AddHabit/page";
+import { Plus, X } from "lucide-react";
+import React, { useState } from "react";
 
 export default function Footer() {
+	const [isModalOpen, setIsModalOpen] = useState(false);
+
+	const toggleModal = () => {
+		setIsModalOpen(!isModalOpen);
+	};
+
+	const closeModal = (e: any) => {
+		if (e.target.id === "modal-backdrop") {
+			setIsModalOpen(false);
+		}
+	};
+
 	return (
 		<>
 			<div className="flex justify-center py-4">
-				<Link href={"/AddHabit"}>
-					<button className="bg-orange-400 hover:bg-orange-500 hover:scale-110 animation ease-in duration-200 text-white font-bold py-4 px-4 rounded-[2.0rem]">
-						<Plus />
-					</button>
-				</Link>
+				<button
+					className="bg-orange-400 hover:bg-orange-500 hover:scale-110 animation ease-in duration-200 text-white font-bold py-4 px-4 rounded-[2.0rem]"
+					onClick={toggleModal}
+				>
+					<Plus />
+				</button>
 			</div>
+
+			{isModalOpen && (
+				<div
+					id="modal-backdrop"
+					onClick={closeModal}
+					className="fixed inset-0 bg-gray-800 bg-opacity-50 flex justify-center items-center z-50"
+				>
+					{/* Modal Content */}
+					<div
+						className="bg-white rounded-lg p-6 w-full max-w-md transition-all transform ease-in duration-300"
+						onClick={(e) => e.stopPropagation()}
+					>
+						<button
+							className="absolute top-4 right-4 text-gray-600"
+							onClick={toggleModal}
+						>
+							<X className="w-6 h-6" />
+						</button>
+						<AddHabit /> {/* Render the AddHabit component inside the modal */}
+					</div>
+				</div>
+			)}
+
 			<footer className="bg-gray-100 text-gray-600 p-4 text-center">
 				<div className="container mx-auto">
 					<p>© Copyright {new Date().getFullYear()}. All rights reserved.</p>
